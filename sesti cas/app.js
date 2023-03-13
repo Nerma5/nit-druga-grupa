7049c96ead59086e8229c9f5741daeb019bc9c1b
Nerma5 <nermahh5@gmail.com>
2023-03-14 00:41:47 +0100

    checking for commits

--- sesti cas/app.js
+++ sesti cas/app.js
@@ -14,3 +14,17 @@
 
 // const increment = (prev, curr = 1) => prev + curr;
 // console.log(increment(1, 2));
+
+////////////////////////////////////zadaci sa stringovima///////////////////////////////////////////
+
+let string = "Mama ima momu";
+
+const numbers = (someString) => {
+  let count = 0;
+  for (let i = 0; i < someString.length; i++) {
+    count++;
+  }
+  return count;
+};
+
+console.log(numbers(string));
